43de0b2a520be4a19ace007c085f29ae01789339
Udayan Shevade <udayan.shevade@gmail.com>
2016-07-08 12:52:24 -0400

    Add service to save creation info in case user exits form prematurely

---
+++ app/scripts/services/createDetails.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eventPlannerApp.service:createDetails
+ * @description
+ * # createDetails Service
+ * Controller of the eventPlannerApp
+ */
+angular.module('eventPlannerApp')
+  .service('createDetails', function() {
+
+});

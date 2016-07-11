d19b37854d7f37e802fe4abfa65ac904a354b4cc
Udayan Shevade <udayan.shevade@gmail.com>
2016-07-11 14:15:25 -0400

    Add pre-tests test configuration

---
+++ test/spec/controllers/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+describe('Controller: MainCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('eventPlannerApp'));
+
+  it('should attach a list of awesomeThings to the scope', function () {
+    expect('Defined').toBeDefined();
+  });
+});
